83d7e1e3a2dbf061f931522bcd84d4c00e7106ea
DeyvidA <deyvida1001@gmail.com>
2022-01-17 14:32:44 -0600

    final modulo 2

--- cursoBasico_vuejs2/app.js
+++ cursoBasico_vuejs2/app.js
@@ -1,57 +1,121 @@
-new Vue ({
-    el: '#app',
+Vue.component('coinDetail', {
+
+    props: ['coin'], 
 
     data () {
         return {
-            name: 'Bitcoin',
-            Symbol: 'BTC',
-            img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
-            changePercent: 0,
-
-            value: 0,
-
-            color: 'f4f4f4',
-
-            price: 8400,
-
-            pricesWithDays: [
-                { day: 'Lunes', value: 8400 },
-                { day: 'Martes', value: 7900 },
-                { day: 'Miercoles', value: 8200 },
-                { day: 'Jueves', value: 9000 },
-                { day: 'Viernes', value: 9400 },
-                { day: 'Sabado', value: 10000 },
-                { day: 'Domingo', value: 10200 },
-            ],
-
-            showPrices: false
+            showPrices: false,
+            value: 0
         }
     },
 
     computed: {
         title() {
-            return `${this.name} - ${this.Symbol}`
+            return `${this.coin.name} - ${this.coin.Symbol}`
         },
+
         convertedValue () {
             if (!this.value) {
                 return 0
             }
 
-            return this.value / this.price;
+            return this.value / this.coin.price;
         },
     },
 
-    watch: {
-        showPrices (newValue, oldValue) {
-            console.log(newValue, oldValue)
+    method: {
+        toggleShowPrices() {
+            this.showPrices = !this.toggleShowPrices
+
+            this.$emit('change-color', 
+            this.showPrices ? 'FF96C8' : '3D3D3D')
+            // this.color = this.color.split('').reverse().join('');
         }
     },
 
-    methods: {
-        toggleShowPrices() {
-            this.showPrices = !this.showPrices;
+    created () {
+        console.log('Mounted...');
+    },
+    mounted () {
+        console.log('Mounted...');
+    },
+    template: `
+        <div>
+            <img 
+                v-on:mouseover="toggleShowPrices"
+                v-on:mouseout="toggleShowPrices"
+                v-bind:src="coin.img" v-bind:alt="coin.name"
+            />
 
-            this.color = this.color.split('').reverse().join('');
+
+            <h1 v-bind:class="coin.changePercent > 0 ? 'verde' : 'rojo' ">
+                {{ title }}
+                <span v-if="coin.changePercent > 0">😄</span>
+                <span v-else-if="coin.changePercent < 0">F</span>
+                <span v-else>eu</span>
+
+                <span v-on:click="toggleShowPrices">
+                    {{ showPrices ? 'siu' : 'nou' }}
+                </span>
+            </h1>
+
+            <input type="number" v-model="value">
+            <span> {{ convertedValue }}</span>
+
+            <slot name="text"></slot>
+            <slot name="link"></slot>
+
+            <ul v-show="showPrices">     
+                <li
+                class="mayuscula"
+                v-bind:class="{ naranja: p.value == coin.price, rojo: p.value <  coin.price, verde: p.value > coin.price}"
+                v-for="(p , i) in coin.pricesWithDays"
+                :key='p.day'>
+                    {{ i }} = {{ p.day }} - {{ p.value }}
+                </li>
+            </ul>
+
+        </div>
+    `
+})
+
+new Vue ({
+    el: '#app',
+
+    data () {
+        return {
+            btc: {
+                name: 'Bitcoin',
+                Symbol: 'BTC',
+                img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
+                changePercent: 0,
+                price: 8400,
+                pricesWithDays: [
+                    { day: 'Lunes', value: 8400 },
+                    { day: 'Martes', value: 7900 },
+                    { day: 'Miercoles', value: 8200 },
+                    { day: 'Jueves', value: 9000 },
+                    { day: 'Viernes', value: 9400 },
+                    { day: 'Sabado', value: 10000 },
+                    { day: 'Domingo', value: 10200 },
+                ],
+            },            
+
+            color: 'f4f4f4',
+        }
+    },
+    created () {
+        console.log('Mounted...');
+    },
+    mounted () {
+        console.log('Mounted...');
+    },
+    methods: {
+        updateColor(color) {
+            this.color = color || this.color
+            .split('')
+            .reverse()
+            .join('');
         }
     }
 })
